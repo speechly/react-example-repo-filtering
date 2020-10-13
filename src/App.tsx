e8848b3c2e28b43aaba81814f5fa713b2e422b8e
Igor Suleymanov <igor@speechly.com>
2020-10-13 20:06:22 +0300

    Use app ID and language from the environment

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,19 @@ import "./App.css";
 import { SpeechApp } from "./SpeechApp";
 
 function App() {
+  const appId = process.env.REACT_APP_APP_ID ?? "";
+  if (appId === undefined) {
+    throw Error("Missing Speechly app ID!");
+  }
+
+  const language = process.env.REACT_APP_LANGUAGE ?? "";
+  if (language === undefined) {
+    throw Error("Missing Speechly app language!");
+  }
+
   return (
     <div className="App">
-      <SpeechProvider appId="your-app-id" language="en-US">
+      <SpeechProvider appId={appId} language={language}>
         <SpeechApp />
       </SpeechProvider>
     </div>
